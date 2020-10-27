ecb51145f8c24e4529df04a1ef1d44a392387730
samuelvarejames <samuelvarejames@gmail.com>
2020-10-27 23:56:54 +0100

    Update test

--- bstack/src/App.test.js
+++ bstack/src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a Segment placeholder when booklist is empty', () => {
   const { getByText } = render(<App />);
   const headerElement = getByText(/"You don't have any book to read at the moment."/h);
   expect(headerElement).toBeInTheDocument();
